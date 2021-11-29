fbda0c39e8b85084cfd0df6fe4a2c7db0f36e3ba
scottti312 <scott.ti312@gmail.com>
2021-11-29 10:22:26 -0800

    added python executor and pass encryption

--- searchAmazon.py
+++ searchAmazon.py
@@ -15,14 +15,17 @@ def search(URL):
     title = soup.find("span", attrs={"id": 'productTitle'})
     title_value = title.string
     title_string = title_value.strip().replace(',', '')
-    print("Title:", title_string)
+    print(title_string)
  
     price_code = soup.findAll("span", attrs={"class": "a-price a-text-price a-size-medium apexPriceToPay"})
     pricea = []
     for p in price_code:
         pricea.append(p.find("span", attrs={"class": "a-offscreen"}))
     price = pricea[0]
-    print('Price:', price.string)
+    price_string = price.string
+    print(price.string)
+
+    return price_string, title_string
 
 url = "https://www.amazon.com/HIFIMAN-Full-Size-Audiophile-Adjustable-Headphone/dp/B07K59HW9R/?_encoding=UTF8&pd_rd_w=dKAIE&pf_rd_p=ed0a22d3-02a9-43b9-a7a8-2632c9eb2ad8&pf_rd_r=BMVWANTXYDRYBY2R497J&pd_rd_r=38ea6469-0b66-49b5-b0f1-0e243a5c4b4e&pd_rd_wg=uyxNX&ref_=pd_gw_ci_mcx_mr_hp_atf_m"
 search(url)
