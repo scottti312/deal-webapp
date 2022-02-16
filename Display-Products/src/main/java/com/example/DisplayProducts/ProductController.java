cf145817e768e589123972f61082ab29380e5d25
aden73 <35171116+aden73@users.noreply.github.com>
2022-02-15 16:03:37 -0800

    display product info on webpage
    Dynamically displays all product info on webpage

--- Display-Products/src/main/java/com/example/DisplayProducts/ProductController.java
+++ Display-Products/src/main/java/com/example/DisplayProducts/ProductController.java
@@ -6,13 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class ProductController {
 
     @GetMapping
     String getProduct(Model model) throws JSONException {
         JSONObject product_data[] = ProductInfo.products();
-        model.addAttribute("product_name", product_data[2].getString("name"));
+
+        model.addAttribute("vendors", product_data);
 
         return "results_page";
     }
