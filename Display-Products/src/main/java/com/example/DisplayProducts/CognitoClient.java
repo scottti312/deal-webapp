e1701ebbf76c95bef8b9228d680329c9e37bef8d
scott <scott.ti312@gmail.com>
2022-05-26 20:06:51 -0700

    removed useless file

--- Display-Products/src/main/java/com/example/DisplayProducts/CognitoClient.java
+++ Display-Products/src/main/java/com/example/DisplayProducts/CognitoClient.java
@@ -28,7 +28,7 @@ public class CognitoClient {
     }
 
     private AWSCognitoIdentityProvider createCognitoClient() {
-        AWSCredentials cred = new BasicAWSCredentials("accesskey", "secretKey");
+        AWSCredentials cred = new BasicAWSCredentials("accessKey", "secretKey");
         AWSCredentialsProvider credProvider = new AWSStaticCredentialsProvider(cred);
         return AWSCognitoIdentityProviderClientBuilder.standard()
                 .withCredentials(credProvider)
